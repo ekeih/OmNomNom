995d2d6b98621b05f70f667c6adea18919804b8d
Max Rosin <github@hackrid.de>
2017-07-03 16:45:57 +0200

    new frontend

---
+++ frontend/frontend.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2017  Max Rosin
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import logging
+import os
+import redis
+import sys
+
+from telegram import ChatAction, ParseMode
+from telegram.ext import CommandHandler, RegexHandler, Updater
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
+
+redis_host = os.environ.get('OMNOMNOM_REDIS_HOST') or 'localhost'
+cache = redis.Redis(host=redis_host, decode_responses=True)
+
+token = os.environ.get('OMNOMNOM_AUTH_TOKEN')
+if not token:
+    logging.error('You have to set your auth token as environment variable in TELEGRAM_BOT_AUTH_TOKEN')
+    sys.exit()
+
+ABOUT_TEXT = """*OmNomNom*
+
+OmNomNom is a Telegram bot to get canteen information. Currently it supports only canteens in Berlin (Germany) \
+and most of its answers are in German.
+The "official" version of this bot is available as [@OmnBot](https://telegram.me/OmnBot). Feel free to talk to it and \
+invite it to your groups.
+Find out more about it on [Github](https://github.com/ekeih/OmNomNom). Pull requests and issues are always welcome. If \
+you have questions you can talk to me via [Telegram](https://telegram.me/ekeih).
+
+OmNomNom is licensed under the [GNU AGPL v3](https://github.com/ekeih/OmNomNom#license).
+"""
+
+logger.debug('Initialize API')
+updater = Updater(token=token)
+dispatcher = updater.dispatcher
+
+
+def __log_incoming_messages(bot, update):
+    chat = update.message.chat
+    target_chat = ''
+    if chat.type == 'group':
+        target_chat = chat.title
+    elif chat.type == 'private':
+        if chat.first_name:
+            target_chat += chat.first_name
+        if chat.last_name:
+            target_chat += ' %s' % chat.last_name
+        if chat.username:
+            target_chat += ' (%s)' % chat.username
+    logger.info('In:  %s: %s' % (target_chat, update.message.text))
+
+
+def __send_typing_action(bot, update):
+    logger.debug("Send typing")
+    bot.sendChatAction(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+
+
+def __about(bot, update):
+    update.message.reply_text(text=ABOUT_TEXT, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+
+def __error_handler(bot, update, error):
+    logger.info(error)
+
+
+def __menu(bot, update):
+    if update.message.text:
+        requested_canteen = update.message.text[1:]
+        logger.debug('Requested Canteen: %s' % requested_canteen)
+        reply = cache.get(requested_canteen) or \
+            'Leider kenne ich keinen passenden Speiseplan. Wenn das ein Fehler ist, wende dich einfach an @ekeih.'
+        logger.debug(reply)
+        update.message.reply_text(text=reply, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+logger.debug('Adding API callbacks')
+dispatcher.add_error_handler(__error_handler)
+dispatcher.add_handler(CommandHandler('start', __about), 2)
+dispatcher.add_handler(CommandHandler('about', __about), 2)
+dispatcher.add_handler(CommandHandler('help', __about), 2)
+dispatcher.add_handler(RegexHandler('.*', __send_typing_action), 0)
+dispatcher.add_handler(RegexHandler('.*', __log_incoming_messages), 1)
+dispatcher.add_handler(RegexHandler('/.*', __menu), 2)
+
+logger.info('Start polling')
+updater.start_polling()
