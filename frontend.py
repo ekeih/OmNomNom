fe9e27ed035e50e2c0c2f089a5479a65fbee08e1
Max Rosin <github@hackrid.de>
2017-08-09 21:45:12 +0200

    send help message to users who send strange messages or when invited to a group

--- frontend.py
+++ frontend.py
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
+import emoji
 import os
 import redis
 import sys
@@ -24,7 +25,7 @@ import textwrap
 from omnomgram.tasks import send_message_to_admin
 from stats.tasks import log_to_influxdb
 from telegram import Bot, ChatAction, ParseMode
-from telegram.ext import CommandHandler, RegexHandler, Updater
+from telegram.ext import CommandHandler, Filters, MessageHandler, RegexHandler, Updater
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -57,6 +58,26 @@ you have questions you can talk to me via [Telegram](https://telegram.me/ekeih).
 
 OmNomNom is licensed under the [GNU AGPL v3](https://github.com/ekeih/OmNomNom#license).
 """
+
+HELP_TEXT = """\
+            *OmNomNom - Hilfe*
+
+            Hallo,
+
+            für jede Mensa gibt es einen Befehl, den du mir schicken kannst.
+
+            Für die Mensa der TU-Berlin ist das zum Beispiel: /tu\_marchstr.
+
+            Alle verfügbaren Mensen und andere Befehle (wie zum Beispiel /help oder /about) findest du über die Auto-Vervollständigung von Telegram, wenn du anfängst eine Nachricht zu tippen, die mit `/` beginnt.
+            Außerdem gibt es in den meisten Telegram-Clients neben dem Textfeld einen viereckigen Button, der einen `/` enthält, über den du alle verfügbaren Befehle auswählen kannst.
+
+            Übrigens kannst du mich auch in Gruppen einladen, sodass mich dort jeder nach den Speiseplänen fragen kann.
+
+            Viel Spaß und guten Appetit! %s
+            Bei Problemen sprich einfach @ekeih an.
+
+            P.S.: Der Bot ist OpenSource (GNU AGPL v3) und den Code findest du auf [GitHub](https://github.com/ekeih/OmNomNom). %s
+            """ % (emoji.emojize(':cake:', use_aliases=True), emoji.emojize(':smile:', use_aliases=True))
 
 logger.debug('Initialize API')
 updater = Updater(token=token)
@@ -132,6 +153,7 @@ def __menu(bot, update):
         logger.debug(reply)
         update.message.reply_text(text=reply, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
+
 def __deprecated_commands(bot, update):
     requested_canteen = update.message.text[1:].replace(bot.name, '')
     if requested_canteen == 'tu_mar':
@@ -144,16 +166,23 @@ def __deprecated_commands(bot, update):
     send_message_to_admin('%s\n\n`%s`' % (reply, update.effective_user))
     update.message.reply_text(text=reply, parse_mode=ParseMode.MARKDOWN)
 
+
+def __help(bot, update):
+    update.message.reply_text(text=textwrap.dedent(HELP_TEXT), parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+
+
 logger.debug('Adding API callbacks')
 dispatcher.add_error_handler(__error_handler)
-dispatcher.add_handler(CommandHandler('start', __about), 2)
+dispatcher.add_handler(CommandHandler('start', __help), 2)
 dispatcher.add_handler(CommandHandler('about', __about), 2)
-dispatcher.add_handler(CommandHandler('help', __about), 2)
+dispatcher.add_handler(CommandHandler('help', __help), 2)
+dispatcher.add_handler(CommandHandler('join', __help), 2)
 dispatcher.add_handler(RegexHandler('.*', __send_typing_action), 0)
 dispatcher.add_handler(RegexHandler('.*', __log_incoming_messages), 1)
 dispatcher.add_handler(CommandHandler('tu_mar', __deprecated_commands), 2)
 dispatcher.add_handler(CommandHandler('tu_tel', __deprecated_commands), 2)
 dispatcher.add_handler(RegexHandler('/.*', __menu), 2)
+dispatcher.add_handler(MessageHandler(Filters.all, __help), 2)
 
 start_message = """*Bot Started*
 
