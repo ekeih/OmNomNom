34cb9daaf61a71db7bd657edb44ee8b438fd7716
Max Rosin <github@hackrid.de>
2017-08-19 23:24:00 +0200

    change vegan symbol to seedling to follow online standards

--- canteens/canteen.py
+++ canteens/canteen.py
@@ -2,5 +2,5 @@ import emoji
 
 FISH = emoji.emojize(':fish:')
 MEAT = emoji.emojize(':poultry_leg:')
-VEGAN = emoji.emojize(':herb:')
+VEGAN = emoji.emojize(':seedling:')
 VEGGIE = emoji.emojize(':ear_of_corn:')
