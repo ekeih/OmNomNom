be1ec383ddaade9f29b0dfd98892404075b9b234
Max Rosin <github@hackrid.de>
2017-09-18 17:20:34 +0200

    allow custom refresh times

--- backend/backend.py
+++ backend/backend.py
@@ -15,6 +15,9 @@ cache_database = environ.get('OMNOMNOM_CACHE_DATABASE') or 0
 
 celery_database = environ.get('OMNOMNOM_CELERY_DATABASE') or 1
 
+refresh_minute = environ.get('OMNOMNOM_REFRESH_MINUTE') or 3
+refresh_hour = environ.get('OMNOMNOM_REFRESH_HOUR') or '*/2'
+
 cache = Redis(host=redis_host, port=redis_port, db=cache_database)
 cache_date_format = '%Y-%m-%d'
 
@@ -33,23 +36,23 @@ app.conf.timezone = 'Europe/Berlin'
 app.conf.beat_schedule = {
     'update cafenero': {
         'task': 'canteens.cafenero.update_cafenero',
-        'schedule': crontab(minute=3)
+        'schedule': crontab(hour=refresh_hour, minute=refresh_minute)
     },
     'update singh': {
         'task': 'canteens.singh.update_singh',
-        'schedule': crontab(minute=3)
+        'schedule': crontab(hour=refresh_hour, minute=refresh_minute)
     },
     'update personalkantine': {
         'task': 'canteens.personalkantine.update_personalkantine',
-        'schedule': crontab(minute=3)
+        'schedule': crontab(hour=refresh_hour, minute=refresh_minute)
     },
     'update en canteen': {
         'task': 'canteens.personalkantine.update_en_canteen',
-        'schedule': crontab(minute=3)
+        'schedule': crontab(hour=refresh_hour, minute=refresh_minute)
     },
     'update studierendenwerk': {
         'task': 'canteens.studierendenwerk.update_all_studierendenwerk_canteens',
-        'schedule': crontab(minute=3)
+        'schedule': crontab(hour=refresh_hour, minute=refresh_minute)
     }
 }
 
