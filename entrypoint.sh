2fac53910201cca368b04318538f88216eea2e50
Max Rosin <github@hackrid.de>
2017-09-18 11:19:47 +0200

    fix flower entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ case "$1" in
     exec celery -A backend.backend beat -l info
     ;;
   "flower")
-    exec celery flower -A backend.backend --address=0.0.0.0 --broker="redis://${OMNOMNOM_REDIS_HOST:}:6379/1"
+    exec celery flower -A backend.backend --address=0.0.0.0 --broker="redis://${OMNOMNOM_REDIS_HOST}:6379/1"
     ;;
   "frontend")
     exec python3 frontend.py
