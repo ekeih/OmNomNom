ffa823e412bc45634dcb250fa728db35548354dd
Max Rosin <github@hackrid.de>
2017-10-08 00:03:59 +0200

    tests for canteen week range functions

---
+++ tests/test_canteen.py
@@ -0,0 +1,49 @@
+import pytest
+import datetime
+
+from canteens import canteen
+
+
+@pytest.mark.freeze_time('2017-09-28')
+def test_get_current_week():
+    expected_range = [
+        datetime.date(2017, 9, 25),
+        datetime.date(2017, 9, 26),
+        datetime.date(2017, 9, 27),
+        datetime.date(2017, 9, 28),
+        datetime.date(2017, 9, 29),
+        datetime.date(2017, 9, 30),
+        datetime.date(2017, 10, 1)
+    ]
+    current_week = canteen.get_current_week()
+    assert current_week == expected_range
+
+
+@pytest.mark.freeze_time('2017-09-28')
+def test_next_week():
+    expected_range = [
+        datetime.date(2017, 10, 2),
+        datetime.date(2017, 10, 3),
+        datetime.date(2017, 10, 4),
+        datetime.date(2017, 10, 5),
+        datetime.date(2017, 10, 6),
+        datetime.date(2017, 10, 7),
+        datetime.date(2017, 10, 8)
+    ]
+    next_week = canteen.get_next_week()
+    assert next_week == expected_range
+
+
+@pytest.mark.freeze_time('2017-09-28')
+def test_get_week_range():
+    expected_range = [
+        datetime.date(2017, 9, 25),
+        datetime.date(2017, 9, 26),
+        datetime.date(2017, 9, 27),
+        datetime.date(2017, 9, 28),
+        datetime.date(2017, 9, 29),
+        datetime.date(2017, 9, 30),
+        datetime.date(2017, 10, 1)
+    ]
+    week_range = canteen.get_week_range(datetime.date(2017, 10, 1))
+    assert week_range == expected_range
