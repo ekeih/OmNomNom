b8b4eec56687c5b25bc58ac651138970b2c2fb95
Max Rosin <github@hackrid.de>
2017-10-12 23:07:43 +0200

    change frontend path in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ case "$1" in
     exec celery flower -A backend.backend --address=0.0.0.0 --broker="redis://${OMNOMNOM_REDIS_HOST}:6379/1"
     ;;
   "frontend")
-    exec python3 frontend.py
+    exec python3 frontend/frontend.py
     ;;
   "worker")
     exec celery -A backend.backend worker -l info -Q canteens
